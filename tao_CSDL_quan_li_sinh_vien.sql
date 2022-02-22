b5c01a4f661f31eafc71b818543fa5d8ef5be6e4
ruukichi <ngovietlc93@gmail.com>
2022-02-22 14:23:23 +0700

    add

--- tao_CSDL_quan_li_sinh_vien.sql
+++ tao_CSDL_quan_li_sinh_vien.sql
@@ -1,34 +1,24 @@
-create database QuanLiSinhVien;
-use QuanLiSinhVien;
-create table Class(
-classID int Not null auto_increment primary key,
-className varchar(60) not null,
-StartDate datetime not null,
-Status bit
-);
-create table Student(
-StudentID int not null auto_increment primary key,
-StudentName varchar(30) not null,
-Address varchar(50),
-Phone varchar(20),
-Status bit,
-ClassID int not null,
-foreign key(ClassID) references Class(ClassID)
-);
-create table Subject(
-SubID int not null auto_increment primary key,
-SubName varchar(30) not null,
-Credit tinyint not null default 1 check (Credit >=1),
-Status bit default 1
-);
-create table Mark(
-MarkID int not null auto_increment primary key,
-SubID int not null,
-StudentID int not null,
-Mark float default 0 check (Mark between 0 and 100),
-ExamTimes tinyint default 1,
-unique (SubID,StudentID),
-foreign key(subID) references Subject (SubID),
-foreign key(StudentID)references Student (StudentID)
-);
-	
+Use Quanlisinhvien;
+insert into class 
+value (1, 'A1','2008-12-20',1);
+insert into class
+value (2,'A2','2008-12-22',1);
+insert into class
+value (3,'B3',current_date,0);
+insert into student(StudentName,Address,Phone,Status,classId)
+value('Hung','Ha Noi','0912113113',1,1);
+insert into student(StudentName,Address,Status,classId)
+value('Hoa','Hai Phong',1,1);
+insert into student(StudentName,Address,Phone,Status,classId)
+value('Manh','HCM','0123123123',0,2);
+insert into Subject
+value (1,'CF',5,1),
+(2, 'C', 6, 1),
+(3, 'HDJ', 5, 1),
+(4, 'RDBMS', 10, 1)
+;
+Insert into Mark (SubID,StudentID,mark,ExamTimes)
+value(1,1,8,1),
+(1,2,10,2),
+(2,1,12,1)
+;
